ecead16808f7640ffdcb5e763a1573d11cdba069
emilylime <emilyyyylime+git@gmail.com>
2024-09-24 09:56:43 +0300

    Switch document list to use indexmap::IndexMap (added dependency)

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -20,6 +20,7 @@ use helix_vcs::DiffProviderRegistry;
 use futures_util::stream::select_all::SelectAll;
 use futures_util::{future, StreamExt};
 use helix_lsp::{Call, LanguageServerId};
+use indexmap::IndexMap;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 use std::{
@@ -1181,7 +1182,7 @@ pub struct Editor {
     pub mode: Mode,
     pub tree: Tree,
     pub next_document_id: DocumentId,
-    pub documents: BTreeMap<DocumentId, Document>,
+    pub documents: IndexMap<DocumentId, Document>,
 
     // We Flatten<> to resolve the inner DocumentSavedEventFuture. For that we need a stream of streams, hence the Once<>.
     // https://stackoverflow.com/a/66875668
@@ -1331,7 +1332,7 @@ impl Editor {
             mode: Mode::Normal,
             tree: Tree::new(area),
             next_document_id: DocumentId::default(),
-            documents: BTreeMap::new(),
+            documents: IndexMap::new(),
             saves: HashMap::new(),
             save_queue: SelectAll::new(),
             write_count: 0,
@@ -1774,7 +1775,7 @@ impl Editor {
                     // Copy `doc.id` into a variable before calling `self.documents.remove`, which requires a mutable
                     // borrow, invalidating direct access to `doc.id`.
                     let id = doc.id;
-                    self.documents.remove(&id);
+                    self.documents.shift_remove(&id);
 
                     // Remove the scratch buffer from any jumplists
                     for (view, _) in self.tree.views_mut() {
@@ -1998,6 +1999,7 @@ impl Editor {
         }
 
         let doc = self.documents.remove(&doc_id).unwrap();
+        self.documents.shift_remove(&doc_id);
 
         // If the document we removed was visible in all views, we will have no more views. We don't
         // want to close the editor just for a simple buffer close, so we need to create a new view
