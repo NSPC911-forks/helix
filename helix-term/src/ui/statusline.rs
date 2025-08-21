6f4f1fc68e900380f03ed724f719637c2fe2b449
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-08-21 21:04:32 +0800

    patchy: Mergey 13666/statusline-theme

--- helix-term/src/ui/statusline.rs
+++ helix-term/src/ui/statusline.rs
@@ -69,6 +69,7 @@ pub fn render(context: &mut RenderContext, viewport: Rect, surface: &mut Surface
     for element_id in &config.statusline.left {
         let render = get_render_function(*element_id);
         (render)(context, |context, span| {
+            let base_style = statusline_style(context, &element_id.to_string());
             append(&mut context.parts.left, span, base_style)
         });
     }
@@ -85,6 +86,7 @@ pub fn render(context: &mut RenderContext, viewport: Rect, surface: &mut Surface
     for element_id in &config.statusline.right {
         let render = get_render_function(*element_id);
         (render)(context, |context, span| {
+            let base_style = statusline_style(context, &element_id.to_string());
             append(&mut context.parts.right, span, base_style)
         })
     }
@@ -104,6 +106,7 @@ pub fn render(context: &mut RenderContext, viewport: Rect, surface: &mut Surface
     for element_id in &config.statusline.center {
         let render = get_render_function(*element_id);
         (render)(context, |context, span| {
+            let base_style = statusline_style(context, &element_id.to_string());
             append(&mut context.parts.center, span, base_style)
         })
     }
@@ -160,6 +163,22 @@ where
         helix_view::editor::StatusLineElement::VersionControl => render_version_control,
         helix_view::editor::StatusLineElement::Register => render_register,
         helix_view::editor::StatusLineElement::CurrentWorkingDirectory => render_cwd,
+    }
+}
+
+fn statusline_style(context: &RenderContext, scope: &str) -> Style {
+    let scope = if context.focused {
+        format!("ui.statusline.{scope}")
+    } else {
+        format!("ui.statusline.inactive.{scope}")
+    };
+
+    let config = context.editor.config();
+
+    if config.color_modes {
+        context.editor.theme.get(&scope)
+    } else {
+        Style::default()
     }
 }
 
@@ -444,7 +463,7 @@ where
     let maxrows = context.doc.text().len_lines();
     write(
         context,
-        format!("{}%", (position.row + 1) * 100 / maxrows).into(),
+        format!(" {}% ", (position.row + 1) * 100 / maxrows).into(),
     );
 }
 
@@ -538,11 +557,7 @@ fn render_file_modification_indicator<'a, F>(context: &mut RenderContext<'a>, wr
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
-    let title = if context.doc.is_modified() {
-        "[+]"
-    } else {
-        "   "
-    };
+    let title = if context.doc.is_modified() { "[+]" } else { "" };
 
     write(context, title.into());
 }
@@ -579,13 +594,9 @@ fn render_separator<'a, F>(context: &mut RenderContext<'a>, write: F)
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
-    let style = context.editor.theme.get("ui.statusline.separator");
 
-    let icons = ICONS.load();
-
-    let separator = icons.ui().statusline().separator().to_string();
-
-    write(context, Span::styled(separator, style));
+    let sep = context.editor.config().statusline.separator.clone();
+    write(context, sep.into());
 }
 
 fn render_spacer<'a, F>(context: &mut RenderContext<'a>, write: F)
@@ -626,11 +637,10 @@ fn render_file_indent_style<'a, F>(context: &mut RenderContext<'a>, write: F)
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
-    let style = context.doc.indent_style;
-
+    let indent_style = context.doc.indent_style;
     write(
         context,
-        match style {
+        match indent_style {
             IndentStyle::Tabs => " tabs ".into(),
             IndentStyle::Spaces(indent) => {
                 format!(" {} space{} ", indent, if indent == 1 { "" } else { "s" }).into()
