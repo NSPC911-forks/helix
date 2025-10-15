3915d6fe81bf1bdbdd073259bcd4538ea6f95cce
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-10-15 17:05:22 +0800

    path i think

--- helix-term/src/ui/mod.rs
+++ helix-term/src/ui/mod.rs
@@ -35,6 +35,7 @@ use helix_view::Editor;
 use tui::text::{Span, Spans, ToSpan};
 use tui::widgets::Cell;
 
+use std::borrow::Cow;
 use std::path::Path;
 use std::{error::Error, path::PathBuf};
 
@@ -203,6 +204,22 @@ fn get_excluded_types() -> ignore::types::Types {
         .build()
         .expect("failed to build excluded_types")
 }
+/// Get the relative directory as a string.
+///
+/// NOTE: Assumes the given path is a directory, and will always output with a
+/// trailing slash.
+fn get_relative_dir(path: &Path) -> Cow<'static, str> {
+    let path = helix_stdx::path::get_relative_path(path);
+    if path.components().next().is_none() {
+        "./".into()
+    } else {
+        let mut str = path.to_string_lossy().into_owned();
+        if !str.ends_with('/') {
+            str.push('/');
+        }
+        str.into()
+    }
+}
 
 #[derive(Debug)]
 pub struct FilePickerData {
@@ -253,7 +270,7 @@ pub fn file_picker(editor: &Editor, root: PathBuf) -> FilePicker {
     log::debug!("file_picker init {:?}", Instant::now().duration_since(now));
 
     let columns = [PickerColumn::new(
-        "path",
+        get_relative_dir(&root),
         |item: &PathBuf, data: &FilePickerData| {
             let path = item.strip_prefix(&data.root).unwrap_or(item);
             let mut spans = Vec::with_capacity(4);
@@ -288,6 +305,7 @@ pub fn file_picker(editor: &Editor, root: PathBuf) -> FilePicker {
             cx.editor.set_error(err);
         }
     })
+    .always_show_headers()
     .with_preview(|_editor, path| Some((path.as_path().into(), None)));
     let injector = picker.injector();
     let timeout = std::time::Instant::now() + std::time::Duration::from_millis(30);
@@ -321,7 +339,7 @@ pub fn file_explorer(root: PathBuf, editor: &Editor) -> Result<FileExplorer, std
     let directory_content = directory_content(&root, editor)?;
 
     let columns = [PickerColumn::new(
-        "path",
+        get_relative_dir(&root),
         |(path, is_dir): &(PathBuf, bool), (root, directory_style): &(PathBuf, Style)| {
             let name = path.strip_prefix(root).unwrap_or(path).to_string_lossy();
 
@@ -373,6 +391,7 @@ pub fn file_explorer(root: PathBuf, editor: &Editor) -> Result<FileExplorer, std
             }
         },
     )
+    .always_show_headers()
     .with_preview(|_editor, (path, _is_dir)| Some((path.as_path().into(), None)));
 
     Ok(picker)
