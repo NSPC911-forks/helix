010090209583c9babd4819258ce6684df791598f
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-11-12 01:26:24 +0800

    patchy: Merge 14118/rainbow-indent-guides

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -948,6 +948,7 @@ pub struct IndentGuidesConfig {
     pub render: bool,
     pub character: char,
     pub skip_levels: u8,
+    pub rainbow: bool,
 }
 
 impl Default for IndentGuidesConfig {
@@ -956,6 +957,7 @@ impl Default for IndentGuidesConfig {
             skip_levels: 0,
             render: false,
             character: '│',
+            rainbow: false,
         }
     }
 }
