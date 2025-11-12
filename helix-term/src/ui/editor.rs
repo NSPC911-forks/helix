81be31ccef13c7e2083ce5c9df99d9caccd150fc
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-11-12 18:34:53 +0800

    tysm warp

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -242,7 +242,7 @@ impl EditorView {
                 Center,
             ),
             empty_line(),
-        ];;
+        ];
 
         debug_assert!(
             raw_help_lines.len() >= LOGO_STR.lines().count(),
--- helix-term/src/ui/statusline.rs
+++ helix-term/src/ui/statusline.rs
@@ -590,12 +590,14 @@ fn render_separator<'a, F>(context: &mut RenderContext<'a>, write: F)
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
-    let sep = &context.editor.config().statusline.separator;
     let style = context.editor.theme.get("ui.statusline.separator");
 
-    write(context, Span::styled(sep.to_string(), style));
-}
+    let icons = ICONS.load();
 
+    let separator = icons.ui().statusline().separator().to_string();
+
+    write(context, Span::styled(separator, style));
+}
 fn render_spacer<'a, F>(context: &mut RenderContext<'a>, write: F)
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
